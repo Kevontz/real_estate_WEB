c508357277f68e4b5b533e1182e0ce6a83d005a1
GustavoTamezava0206 <144955610+GustavoTamezava0206@users.noreply.github.com>
2024-06-14 17:20:33 -0300

    Update login.component.ts
    funções

--- real-estate-frontend/src/app/pages/login/login.component.ts
+++ real-estate-frontend/src/app/pages/login/login.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  errorMessage: string = '';
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.apiService.login(email, password).subscribe(
+        response => {
+          console.log('Resposta do login:', response);
+        
+          this.router.navigate(['/']);
+        },
+        error => {
+          console.error('Erro ao fazer login:', error);
+          this.errorMessage = 'Erro ao fazer login. Verifique suas credenciais.';
+        }
+      );
+    } else {
+      this.markFormGroupTouched(this.loginForm);
+    }
+  }
+
+  markFormGroupTouched(formGroup: FormGroup): void {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+    });
+  }
+
+ 
+  getErrors(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    if (control?.errors) { 
+      if (control.errors['required'] && control.touched) {
+        return 'Campo obrigatório.';
+      } else if (control.errors['email'] && control.touched) {
+        return 'Email inválido.';
+      }
+    }
+    return '';
+  }
+  
 }
