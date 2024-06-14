8305c5e16c8ac09c4ed1c4f447f421fef311c80c
GustavoTamezava0206 <144955610+GustavoTamezava0206@users.noreply.github.com>
2024-06-14 17:39:38 -0300

    Update signup.component.ts
    cadastro

--- real-estate-frontend/src/app/pages/signup/signup.component.ts
+++ real-estate-frontend/src/app/pages/signup/signup.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder, private api : ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const user = {
+        username: this.registerForm.value.username,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password
+      };
+
+      this.api.register(user).subscribe(
+        response => {
+          console.log('Registro bem-sucedido!', response);
+          this.router.navigate(['/']);
+        },
+        error => {
+          console.error('Erro no registro:', error);
+        
+        }
+      );
+    } else {
+    
+      Object.values(this.registerForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+  }
 }
